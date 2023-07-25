22e54c5e22aa1b52d18762113e7fe28988fcee46
Laryze Lozano <95232077+Larz-Lozano@users.noreply.github.com>
2023-07-25 12:17:28 +0800

    Update Lozano_Cotibar.py

--- Lozano_Cotibar.py
+++ Lozano_Cotibar.py
@@ -1,9 +1,9 @@
 # You can start inputting the code
 
-def sum(a, b):
-    return (a + b)
-
-a = int(input('Enter 1st number: '))
-b = int(input('Enter 2nd number: '))
-
-print(f'Sum of {a} and {b} is {sum(a, b)}')
+class Smartphone:
+    def__init__(self,
+            self.color = c
+            self.power = p
+            self.battery = b
+            self.audio = 
+            
