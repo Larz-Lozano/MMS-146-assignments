38efab307c9fc00cefe36c736aadc44000a0622f
Jeremias Cotibar <140481723+Jhaeci02@users.noreply.github.com>
2023-07-25 20:38:08 +0800

    cotibar1

--- Lozano_Cotibar.py
+++ Lozano_Cotibar.py
@@ -1,60 +1,54 @@
-#This is the Smartphone Class
-class Smartphone:
-    def__init__(self,
-            self.color = c
-            self.power = p
-            self.battery = b
-            self.audio = 
-
-
-
 #This is the Laptop Class
-class Laptop
-    def__init__(self,brand,batt,color,bright,backkey,weight,width,os)
-            self.brand = brand
-            self.battery = batt
-            self.color = color
-            self.brightness = bright
-            self.backlit_keyboard = backkey
-            self.weight = weight
-            self.width = width
-            self.os = os
+class Laptop:
+    def __init__(self,brand,batt,color,bright,backkey,weight,width,os):
+        self.brand = brand
+        self.battery = batt
+        self.color = color
+        self.brightness = bright
+        self.backlit_keyboard = backkey
+        self.weight = weight
+        self.width = width
+        self.os = os
 
 #This is the method for the Laptop Class
-    def give_brand
-    self.brand = apple
+    def give_brand(self,brand):
+        self.brand = apple
 
-    def increase_battery
-    self.battery =  self.battery + 1
+    def give_battery(self,batt):
+        self.battery = 100
 
-    def decrease_battery
-    self.battery = self.battery - 1
+    def increase_battery(self,batt):
+        self.battery = self.battery + 1
 
-    def give_color
-    self.color = white
+    def decrease_battery(self,batt):
+        self.battery = self.battery - 1
 
-    def increase_brightness
-    self.brightness = self.brightness + 1
+    def give_color(self,color):
+        self.color = white
 
-    def decrease_brightness
-    self.brightness = self.brightness - 1
+    def increase_brightness(self,bright):
+        self.brightness = self.brightness + 1
 
-    def on_backlit_keyboard
-    self.backlit_keyboard = on
+    def decrease_brightness(self,bright):
+        self.brightness = self.brightness - 1
 
-    def off_backlit_keyboard
-    self.backlit_keyboard = off
+    def on_backlit_keyboard(self,backkey):
+        self.backlit_keyboard = on
 
-    def update_os
-    self.os = update
+    def off_backlit_keyboard(self,backkey):
+        self.backlit_keyboard = off
+
+    def update_os(self,os):
+        self.os = update
 
 #Main Program
-Laptop1 = Laptop (apple,100,white,10,100,200,300,macosx)
+Laptop1 = Laptop("apple",100,"white",10,100,200,300,"macosx")
 
 
-Print (Laptop1.brand)
-Print (Laptop1.bright)
-Print (Laptop1.os)
+print (Laptop1.brand)
+print (Laptop1.battery)
+print (Laptop1.color)
+print (Laptop1.os)
 
 #This is the Book Class
 class Book
