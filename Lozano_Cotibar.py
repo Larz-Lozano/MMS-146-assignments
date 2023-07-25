4696ec5733d98d3c75b098a7f4b786fa5e91f5a9
Laryze Lozano <95232077+Larz-Lozano@users.noreply.github.com>
2023-07-25 17:17:28 +0800

    Update Lozano_Cotibar.py

--- Lozano_Cotibar.py
+++ Lozano_Cotibar.py
@@ -1,9 +1,17 @@
-# You can start inputting the code
-
+# This is Smartphone
 class Smartphone:
-    def__init__(self,
+    def__init__(self, c, p, b, a, r)
             self.color = c
             self.power = p
             self.battery = b
-            self.audio = 
+            self.audio = a
+            self.resolution = r
+
+# This is the method the Smartphone class
+    def give_color(self):
+        self.color = blue
+
+# Main program
+smartphone1 = ("red", "on", 50, 70, 1980 x
+            
             
