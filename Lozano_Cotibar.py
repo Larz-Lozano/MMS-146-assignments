e85ee5067a19573b6c53f49c2a968127622ec24a
Jeremias Cotibar <140481723+Jhaeci02@users.noreply.github.com>
2023-07-25 19:43:05 +0800

    cotibar1

--- Lozano_Cotibar.py
+++ Lozano_Cotibar.py
@@ -1,9 +1,3 @@
-# You can start inputting the code
-#This is the Earphone Class
-class Earphone
-    def__init__()
-    
-
 #This is the Smartphone Class
 class Smartphone:
     def__init__(self,
@@ -11,8 +5,65 @@ class Smartphone:
             self.power = p
             self.battery = b
             self.audio = 
-            
-class StandFan
-     def__init__()
 
-#This is
+
+
+#This is the Laptop Class
+class Laptop
+    def__init__(self,brand,batt,color,bright,backkey,weight,width,os)
+            self.brand = brand
+            self.battery = batt
+            self.color = color
+            self.brightness = bright
+            self.backlit_keyboard = backkey
+            self.weight = weight
+            self.width = width
+            self.os = os
+
+#This is the method for the Laptop Class
+    def give_brand
+    self.brand = apple
+
+    def increase_battery
+    self.battery =  self.battery + 1
+
+    def decrease_battery
+    self.battery = self.battery - 1
+
+    def give_color
+    self.color = white
+
+    def increase_brightness
+    self.brightness = self.brightness + 1
+
+    def decrease_brightness
+    self.brightness = self.brightness - 1
+
+    def on_backlit_keyboard
+    self.backlit_keyboard = on
+
+    def off_backlit_keyboard
+    self.backlit_keyboard = off
+
+    def update_os
+    self.os = update
+
+#Main Program
+Laptop1 = Laptop (apple,100,white,10,100,200,300,macosx)
+
+
+Print (Laptop1.brand)
+Print (Laptop1.bright)
+Print (Laptop1.os)
+
+#This is the Book Class
+class Book
+
+#This is the Cabinet Class
+class Cabinet
+
+#This is the Washing Machine Class
+class Washing Machine
+
+#This is the Bed Class
+class Bed
