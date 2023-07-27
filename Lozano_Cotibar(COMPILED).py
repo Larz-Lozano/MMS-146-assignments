4692e5b075c7c7dcc1cb39835fc7d24d369d15ac
Laryze Lozano <95232077+Larz-Lozano@users.noreply.github.com>
2023-07-27 18:54:49 +0800

    Update Lozano_Cotibar(COMPILED).py

--- Lozano_Cotibar(COMPILED).py
+++ Lozano_Cotibar(COMPILED).py
@@ -126,7 +126,7 @@ else:
 print (" ")
 
 
-# THIS IS THE STANDFAN cLASS
+# THIS IS THE STANDFAN CLASS
 class Standfan:
     def __init__(self, brnd, clr, hgt, tlt, spd):
         self.brand = brnd
@@ -187,253 +187,261 @@ elif myStandfan.speed == 3:
     print ("  Speed/Power (after): 3")
 else:
     print ("  Speed/Power (after): Off")
-
+print ("")
 
 #This is the Laptop Class
 class Laptop:
-    def __init__(self,brand,batt,color,bright,backkey,weight,width,os):
-        self.brand = brand
+    def __init__(self,brnd,batt,clr,bght,backkey,wgt,wdth,os):
+        self.brand = brnd
         self.battery = batt
-        self.color = color
-        self.brightness = bright
+        self.color = clr
+        self.brightness = bght
         self.backlit_keyboard = backkey
-        self.weight = weight
-        self.width = width
+        self.weight = wgt
+        self.width = wdth
         self.os = os
 
-#This is the methods for the Laptop Class
-    def give_brand(self,brand):
-        self.brand = brand
+# METHOD FOR LAPTOP CLASS
+    def give_brand(self,n):
+        self.brand = n
 
-    def give_battery(self,batt):
-        self.battery = batt
+    def give_color(self,n):
+        self.color = self.color + n
 
-    def increase_battery(self,batt):
-        self.battery = self.battery + 1
+    def percent_battery(self,n):
+        self.battery = self.battery - n
 
-    def decrease_battery(self,batt):
-        self.battery = self.battery - 1
+    def increase_brightness(self, n):
+        self.brightness = self.brightness + n
 
-    def give_color(self,color):
-        self.color = color
+    def decrease_brightness(self, n):
+        self.brightness = self.brightness - n
 
-    def increase_brightness(self,bright):
-        self.brightness = self.brightness + 1
+    def on_backlit_keyboard(self, n):
+        self.backlit_keyboard = n
 
-    def decrease_brightness(self,bright):
-        self.brightness = self.brightness - 1
-
-    def on_backlit_keyboard(self,backkey):
-        self.backlit_keyboard = backlit_keyboard
-
-    def off_backlit_keyboard(self,backkey):
-        self.backlit_keyboard = backlit_keyboard
-
-    def update_os(self,os):
-        self.os = os
+    def update_os(self,n):
+        self.os = n + "(updated)"
+        
 
 #Main Program
-Laptop1 = Laptop("apple",100,"white",10,100,200,300,"macosx")
+myLaptop = Laptop("apple",100,"white",10,1,200,300,"MACOSN")
 
+print ("4. My Laptop")
+print ("  Brand: " + myLaptop.brand)
+print ("  Battery (before):", myLaptop.battery, "%")
+myLaptop.percent_battery(3)
+print ("  Battery (after):", myLaptop.battery, "%")
+print ("  Color: " + myLaptop.color)
+print ("  OS (before): " + myLaptop.os)
+myLaptop.update_os("MACOSX")
+print ("  OS (after): " + myLaptop.os)
+print ("  Brightness (before):", myLaptop.brightness, "%")
+myLaptop.increase_brightness(5)
+print ("  Brightness (after):", myLaptop.brightness, "%")
+if myLaptop.backlit_keyboard >= 1:
+    print ("  Backlit Keyboard: On")
+else:
+    print ("  Backlit Keyboard: Off")
+print (" ")
 
-print (Laptop1.brand)
-print (Laptop1.battery)
-print (Laptop1.color)
-print (Laptop1.os)
-print ("before increase brightness: ", Laptop1.brightness)
-Laptop1.increase_brightness("0")
-print ("after increase brightness: ", Laptop1.brightness)
-
-
-#This is the Book Class
+# THIS IS BOOK CLASS
 class Book:
-    def __init__(self,title,author,height,weight,width,page):
-        self.title = title
-        self.author = author
-        self.height = height
-        self.weight = weight
-        self.width = width
-        self.page = page
+    def __init__(self,ttl,aut,hgt,wgt,wdt,pg):
+        self.title = ttl
+        self.author = aut
+        self.height = hgt
+        self.weight = wgt
+        self.width = wdt
+        self.page = pg
 
-#This is the methods for the Book Class
-    def give_title(self,title):
-        self.title = title
+# METHOD OF BOOK CLASS
+    def give_title(self,n):
+        self.title = n
 
-    def name_author(self,author):
-        self.author = author
+    def name_author(self,n):
+        self.author = n
 
-    def give_height(self,height):
-        self.height = height
+    def give_height(self,n):
+        self.height = n
 
-    def give_weight(self,weight):
-        self.weight = weight
+    def give_weight(self,n):
+        self.weight = n
 
-    def give_width(self,width):
-        self.width = width
+    def give_width(self,n):
+        self.width = n
 
-    def next_page(self,page):
+    def next_page(self):
         self.page = self.page + 1
 
-    def previous_page(self,page):
+    def previous_page(self):
         self.page = self.page - 1
 
-    def bookmark_page(self,page):
-        self.page = page
-
-    def give_page(self,page):
-        self.page = page
+    def bookmarked_page(self,n):
+        self.page = n
 
 #Main Program
-Book1 = Book("harrypotter","jkrowling",7,101,5,300)
+favBook = Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 9.5, 600, 5, 300)
 
+print ("5. Favorite Book")
+print ("  Title: " + favBook.title)
+print ("  Author: " + favBook.author)
+print ("  Current Page:", favBook.page)
+favBook.next_page()
+print ("  Next Page:", favBook.page)
+favBook.previous_page()
+print ("  Previous Page:", favBook.page)
+favBook.bookmarked_page(205)
+print ("  Bookmarked Page:", favBook.page)
+print (" ")
 
-print (Book1.title)
-print (Book1.author)
-print (Book1.height)
-print (Book1.page)
-print ("before next page: ", Book1.page)
-Book1.next_page("0")
-print ("after next page: ", Book1.page)
-    
-
-#This is the Cabinet Class
+# THIS IS CABINET CLASS
 class Cabinet:
-    def __init__(self,color,door,clothes,height,width,weight):
-        self.color = color
-        self.door = door
-        self.clothes = clothes
-        self.height = height
-        self.width = width
-        self.weight = weight
+    def __init__(self,clr,dr,clth,hgt,wdt,wgt):
+        self.color = clr
+        self.door = dr
+        self.clothes = clth
+        self.height = hgt
+        self.width = wdt
+        self.weight = wgt
 
 #This is the methods for the Cabinet Class
-    def give_color(self,color):
-        self.color = color
+    def give_color(self,n):
+        self.color = n
 
-    def unlock_door(self,door):
-        self.door = door
+    def lock_door(self,n):
+        self.door = n
 
-    def lock_door(self,door):
-        self.door = door
+    def give_height(self,n):
+        self.height = n
 
-    def give_height(self,height):
-        self.height = height
+    def give_width(self,n):
+        self.width = n
 
-    def give_width(self,width):
-        self.width = width
+    def give_weight(self,n):
+        self.weight = n
 
-    def give_weight(self,weight):
-        self.weight = weight
+    def add_clothes(self, n):
+        self.clothes = self.clothes + n
 
-    def add_clothes(self,clothes):
-        self.clothes = self.clothes + 1
-
-    def remove_clothes(self,clothes):
-        self.clothes = self.clothes - 1
+    def remove_clothes(self,n):
+        self.clothes = self.clothes - n
 
 #Main Program
-Cabinet1 = Cabinet("brown","normal",1,30,20,80)
+Cabinet1 = Cabinet("Brown","Unlocked",5,10,30,80)
 
-print (Cabinet1.color)
-print (Cabinet1.door)
-print (Cabinet1.height)
-print (Cabinet1.weight)
-print ("before lock door: ", Cabinet1.door)
-Cabinet1.lock_door("locked")
-print ("after lock door: ", Cabinet1.door)    
+print ("6. My Cabinet")
+print ("  Color:" + Cabinet1.color)
+print ("  Height:", Cabinet1.height, "inches")
+print ("  Weight:", Cabinet1.weight, "kilograms")
+print ("  No. of Clothes:", Cabinet1.clothes)
+Cabinet1.add_clothes(4)
+print ("  No. of Clothes Added:", Cabinet1.clothes)
+Cabinet1.remove_clothes(3)
+print ("  No. of Clothes Removed:", Cabinet1.clothes)
+print ("  Door (before):", Cabinet1.door)
+Cabinet1.lock_door("Locked")
+print ("  Door (after):", Cabinet1.door)   
+print (" ")
 
 
-#This is the Washing Machine Class
+#THIS IS WASHING MACHINE CLASS
 class WashingMachine:
-    def __init__(self,timer,water,color,laundry,program):
-        self.timer = timer
-        self.water = water
-        self.color = color
-        self.laundry = laundry
-        self.program = program
+    def __init__(self,tmr,wtr,clr,ldry,prog):
+        self.timer = tmr
+        self.water = wtr
+        self.color = clr
+        self.laundry = ldry
+        self.program = prog
 
 #This is the methods for the Washing Machine Class
-    def increase_timer(self,timer):
-        self.timer = self.timer + 1
+    def increase_timer(self):
+        self.timer = self.timer + 5
 
-    def add_water(self,water):
-        self.water = self.water + 1
+    def add_water(self,n):
+        self.water = self.water + n
 
-    def drain_water(self,water):
-        self.water = self.water - 1
+    def drain_water(self,n):
+        self.water = self.water - n
 
-    def give_color(self,color):
-        self.color = color
+    def give_color(self,n):
+        self.color = n
 
-    def add_laundry(self,laundry):
-        self.laundry = self.laundry + 1
+    def add_laundry(self,n):
+        self.laundry = self.laundry + n
 
-    def remove_laundry(self,laundry):
-        self.laundry = self.laundry - 1
+    def remove_laundry(self,n):
+        self.laundry = self.laundry - n
 
-    def gentle_program(self,program):
-        self.program = program
-
-    def normal_program(self,program):
-        self.program = program
-
-    def strong_program(self,program):
-        self.program = program
+    def wash_program(self,n):
+        self.program = n
     
 #Main Program
-WashingMachine1 = WashingMachine(60,100,"blue","laundry","normal")
+WashingMachine1 = WashingMachine(35,0,"blue",5, 2)
 
 
-print (WashingMachine1.program)
-print (WashingMachine1.color)
-print ("before add water: ", WashingMachine1.water)
-WashingMachine1.add_water(0)
-print ("after add water: ", WashingMachine1.water)
-print ("before normal program: ", WashingMachine1.program)
-WashingMachine1.normal_program("strong")
-print ("after normal program: ", WashingMachine1.program)
-
+print ("7. Washing Machine")
+print ("  Color: " + WashingMachine1.color)
+print ("  Water (before):", WashingMachine1.water, "gallons")
+WashingMachine1.add_water(6)
+print ("  Water (after):", WashingMachine1.water, "gallons")
+print ("  No. of Laundry (before):", WashingMachine1.laundry)
+WashingMachine1.add_laundry(5)
+print ("  No. of Laundry (after):", WashingMachine1.laundry)
+if WashingMachine1.program ==1:
+    WashingMachine1.program = "Gentle"
+elif WashingMachine1.program == 2:
+    WashingMachine1.program = "Normal"
+elif WashingMachine1.program == 3:
+    WashingMachine1.program = "Strong"
+print ("  Wash Program: " + WashingMachine1.program)
+print (" ")
 
 #This is the Bed Class
 class Bed:
-    def __init__(self,color,width,length,bedsheet,pillows,blanket):
-        self.color = color
-        self.width = width
-        self.length = length
-        self.bedsheet = bedsheet
-        self.pillows = pillows
-        self.blanket = blanket
+    def __init__(self,clr,wdt,lth,bsht,plw,blkt):
+        self.color = clr
+        self.width = wdt
+        self.length = lth
+        self.bedsheet = bsht
+        self.pillows = plw
+        self.blanket = blkt
 
 #This is the methods for the Bed Class
-    def give_color(self,color):
-        self.color = color
+    def give_color(self,n):
+        self.color = n
 
-    def give_width(self,width):
-        self.width = 500
+    def give_width(self,n):
+        self.width = n
 
-    def give_length(self,length):
-        self.length = 150
+    def give_length(self,n):
+        self.length = n
 
-    def change_bedsheet(self,bedsheet):
-        self.bedsheet = bedsheet
+    def change_bedsheet(self,n):
+        self.bedsheet = n
 
-    def add_pillows(self,pillows):
-        self.pillows = self.pillows + 1
+    def add_pillows(self,n):
+        self.pillows = self.pillows + n
 
-    def remove_pillows(self,pillows):
-        self.pillows = self.pillows - 1
+    def remove_pillows(self,n):
+        self.pillows = self.pillows - n
 
-    def change_blanket(self,blanket):
-        self.blanket = blanket
+    def change_blanket(self,n):
+        self.blanket = n
 
 #Main Program
-Bed1 = Bed("gray",500,150,"black",2,"pattern")
+myBed = Bed("Gray",500,150,"Black",2,"Pattern")
 
-
-print (Bed1.color)
-print (Bed1.width)
-print (Bed1.bedsheet)
-print (Bed1.blanket)
-print ("blanket before: ", Bed1.blanket)
-Bed1.change_blanket("stripes")
-print ("blanket after: ", Bed1.blanket)
+print ("8. My Bed")
+print ("  Color: " + myBed.color)
+print ("  Width:", myBed.width, "inches")
+print ("  Length:", myBed.length, "inches")
+print ("  Bedsheet (before): " + myBed.bedsheet)
+myBed.change_bedsheet("White with Green Stripes")
+print ("  Bedsheet (after): " + myBed.bedsheet)
+print ("  Blanket (before): " + myBed.blanket)
+myBed.change_blanket("Plain Green")
+print ("  Blanket (after): ", myBed.blanket)
+print ("  No. of Pillows (before):", myBed.pillows )
+myBed.remove_pillows(1)
+print ("  No. of Pillows (after):", myBed.pillows )
